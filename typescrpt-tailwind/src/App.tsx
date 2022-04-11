e43c342708480bc6a60b7e5ae9a81ec86b3614be
songhoyun <itlockit@gmail.com>
2022-04-11 06:54:42 +0000

    init typescript taildinwcss

---
+++ typescrpt-tailwind/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Hello CodeSandbox</h1>
+      <h2 className="text-center">Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
