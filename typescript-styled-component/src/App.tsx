edc13a918efd185d5c01626e80c24db1f47337d1
dev-ejkim <ejkim@dwhale.kr>
2022-09-28 12:14:11 +0900

    Extract Title from App

--- typescript-styled-component/src/App.tsx
+++ typescript-styled-component/src/App.tsx
@@ -1,12 +1,6 @@
 import styled from "styled-components";
 
 export default function App() {
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
-
   return (
     <div className="App">
       <Title>Hello Clap</Title>
@@ -14,3 +8,10 @@ export default function App() {
     </div>
   );
 }
+
+
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`;
