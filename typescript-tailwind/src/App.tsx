fe2b6dfe73d8dcc59afb91ba6955b1af34f60721
dev-ejkim <ejkim@dwhale.kr>
2022-10-02 15:38:09 +0900

    Update tailwind style

--- typescript-tailwind/src/App.tsx
+++ typescript-tailwind/src/App.tsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello Clap</h1>
-      <h2>Start Coding!!</h2>
+    <div className="text-center mt-10">
+      <h1 className="font-bold text-4xl">Hello Clap</h1>
+      <h2 className="text-pink-400 font-bold text-xl">Start Coding!!</h2>
     </div>
   );
 }
